b87dfd5f6d194062a2bb59d3b1d4799b29565994
Akash Patel <akashp7215@gmail.com>
2020-10-25 18:52:16 -0400

    scripting

--- script.js
+++ script.js
@@ -48,3 +48,32 @@ function initWeather() {
 function storeCityArray() {
     localStorage.setItem("cities", JSON.stringify(cityList));
     }
+
+    // This function saves the currently display city to local storage
+function storeCurrentCity() {
+
+    localStorage.setItem("currentCity", JSON.stringify(cityname));
+}
+      
+
+// Click event handler for city search button
+$("#citySearchBtn").on("click", function(event){
+    event.preventDefault();
+
+    cityname = $("#cityInput").val().trim();
+    if(cityname === ""){
+        alert("Please enter a city to look up")
+
+    }else if (cityList.length >= 5){  
+        cityList.shift();
+        cityList.push(cityname);
+
+    }else{
+    cityList.push(cityname);
+    }
+    storeCurrentCity();
+    storeCityArray();
+    renderCities();
+    displayWeather();
+    displayFiveDayForecast();
+});
